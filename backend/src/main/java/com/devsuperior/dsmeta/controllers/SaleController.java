6066b784264b80dc4104971f0e8f2fc85c18baea
Henrique Borsoi Lopes <henriqueborsoilopes@gmail.com>
2022-07-13 20:36:24 -0300

    API test

---
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dsmeta.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.services.SaleService;
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+	
+	@Autowired
+	private SaleService saleService;
+	
+	@GetMapping
+	public ResponseEntity<List<Sale>> findAllSale(){
+		
+		List<Sale> sales = saleService.findAllSale();
+		
+		return ResponseEntity.ok().body(sales);
+	}
+}
