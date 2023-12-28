3362ce18829081b824c6dcc74366c2c7733e04ca
Henrique Borsoi Lopes <henriqueborsoilopes@gmail.com>
2023-12-28 20:39:44 -0300

    feat(NotificationButton): add axios call

--- frontend/src/component/notification-button/NotificationButton.tsx
+++ frontend/src/component/notification-button/NotificationButton.tsx
@@ -1,10 +1,23 @@
+import axios from 'axios';
 import logo from '../../asset/img/notification-icon.svg';
 import './NotificationButton.css';
 
-export const NotificationButton = () => {
+interface Props {
+  saleId: number;
+}
+
+export const NotificationButton: React.FC<Props> = ({ saleId }) => {
+  function handleClick(saleId: number) {
+    axios
+      .get(`http://localhost:8080/sales/${saleId}/notification`)
+      .then((response) => {
+        console.log('sucesso' + response.data);
+      });
+  }
+
   return (
     <>
-      <div className="dsmeta-red-btn">
+      <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
         <img src={logo} alt="Notificar" />
       </div>
     </>
